3decbbf0a78e3ba1ead3f2b33e5525513deb1738
Oliver Blanthorn <freedom4cows@gmail.com>
2024-11-20 12:44:37 +0100

    Fix min/max of legend

--- src/app.js
+++ src/app.js
@@ -210,7 +210,12 @@ map.on('moveend', () => {
 
 
 function ecdf(array){
-    const mini_array = Array.from({length: Math.min(8192, array.length)}, () => Math.floor(Math.random()*array.length)).map(i => array[i]).sort((l,r) => l-r) // sort() sorts alphabetically otherwise
+    const mini_array = Array.from({length: Math.min(8192, array.length)}, () => Math.floor(Math.random()*array.length)).map(i => array[i]).sort((l,r) => l-r).filter(v => v != null) // sort() sorts alphabetically otherwise
     const quantile = mini_array.map((v, position) => position + 1).map(v => v/mini_array.length) // +=v to weight by number rather than position
-    return [target => quantile[mini_array.findIndex(v => v > target)] ?? 1, target => mini_array[quantile.findIndex(v => Math.min(Math.max(0.01,v),0.99) > target)] ?? mini_array.slice(-1)[0]] // function to get quantile from value and value from quantile, with fudging to exclude top/bottom 1% from legend
+    function getvalue(target) {
+        target = Math.min(Math.max(0.01,target),0.99) // ignore top/bottom 1% because they're usually stupid
+        const v = mini_array[quantile.findIndex(v => v >= target)]
+        return v ?? mini_array[0]
+    }
+    return [target => quantile[mini_array.findIndex(v => v > target)] ?? 1, getvalue] // function to get quantile from value and value from quantile, with fudging to exclude top/bottom 1% from legend
 }
